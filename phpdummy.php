e780cb8dfef6d2befad0ee15d611d8cda07c7d03
roflerikopteri <59827602+roflerikopteri@users.noreply.github.com>
2020-01-31 08:56:54 +0200

    Create phpdummy.php

---
+++ phpdummy.php
@@ -0,0 +1,11 @@
+<?php
+include ("header.html");
+
+$luku1=5;
+$luku2=6;
+$luku3=7;
+
+print "$luku1 = $luku1, $luku2 = $luku2, $luku3 = $luku3, tulo=".($luku1$luku2$luku3); 
+
+include ("footer.html");
+?>
